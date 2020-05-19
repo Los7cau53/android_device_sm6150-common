a12d24b0ed5f5702042174916fa7f15d49a48bbc
AmulyaX <amulya.b520@gmail.com>
2020-05-19 19:47:47 +0000

    sm6150-common: Build vendor.qti.hardware.camera.device@1.0

--- sm6150.mk
+++ sm6150.mk
@@ -151,9 +151,8 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libxml2 \
-    Snap \
-    Gcam \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    vendor.qti.hardware.camera.device@1.0.vendor \
+    vendor.qti.hardware.camera.device@1.0
 
 # CNE
 PRODUCT_PACKAGES += \
